be3258272391a01405a6fd77807cd7092ee34230
sdeepanshu11111 <sdeepanshu1111@gmail.com>
2021-07-04 07:04:34 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 // import styles
 import "./styles/app.scss";
 
+// Material ui
+import { Button } from "@material-ui/core";
+
 // import components
 
+import Nav from "./components/Nav";
+
 import Products from "./components/Products";
+
+import Login from "./components/pages/login";
 
 // import data from
 import Data from "./Data";
@@ -19,6 +26,8 @@ function App() {
 
   return (
     <div className="App">
+      <Nav />
+
       <Products data={data} />
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+const Nav = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Sku finder</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
